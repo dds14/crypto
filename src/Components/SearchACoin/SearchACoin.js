925e7b07a1384716fa765da0dcfcfdfb12e83f27
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-06-12 11:58:39 -0500

    search updated

--- src/Components/SearchACoin/SearchACoin.js
+++ src/Components/SearchACoin/SearchACoin.js
@@ -9,6 +9,7 @@ export class SearchACoin extends Component {
       tickerBeingSearched: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
@@ -16,12 +17,18 @@ export class SearchACoin extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  handleSubmit(e) {
+    console.log(
+      "The coin you are searching for is " + this.state.tickerBeingSearched
+    );
+  }
+
   render() {
     return (
       <div className="entire-search-div">
         <h1>Search Our Universe Of Coins</h1>
         <div>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input
               placeholder="Coin Ticker (ex: BTC)"
               name="tickerBeingSearched"
