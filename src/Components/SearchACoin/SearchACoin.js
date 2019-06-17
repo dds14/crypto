d8f707fd9b392249ea7121387e38cc4d0f68a5b6
Derek Sams <dereksams@dereks-mbp.attlocal.net>
2019-06-16 23:56:59 -0500

    search

--- src/Components/SearchACoin/SearchACoin.js
+++ src/Components/SearchACoin/SearchACoin.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./SearchACoin.scss";
+import axios from "axios";
 
 export class SearchACoin extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      tickerBeingSearched: ""
+      tickerBeingSearched: "",
+      priceOfCoinBeingSearched: 0
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,12 +20,25 @@ export class SearchACoin extends Component {
   }
 
   handleSubmit(e) {
-    console.log(
-      "The coin you are searching for is " + this.state.tickerBeingSearched
-    );
+    axios
+      .get(
+        "https://api.nomics.com/v1/prices?key=5f031f3c4f276ef2b4f4f3fbf2703f08"
+      )
+      .then(response => {
+        console.log("axios call HIT" + this.state.tickerBeingSearched);
+        for (let i = 0; i < response.data.length; i++) {
+          if (response.data.currency == this.state.tickerBeingSearched) {
+            let searchedCoinPrice = response.data[i].price;
+            this.setState({ priceOfCoinBeingSearched: searchedCoinPrice });
+          }
+        }
+      });
   }
 
   render() {
+    console.log(
+      "current state of ticker is: " + this.state.tickerBeingSearched
+    );
     return (
       <div className="entire-search-div">
         <h1>Search Our Universe Of Coins</h1>
@@ -37,6 +52,8 @@ export class SearchACoin extends Component {
             />
             <button className="search-form-submit-button">Search</button>
           </form>
+          <h1>here</h1>
+          <h1>{this.state.priceOfCoinBeingSearched}</h1>
         </div>
       </div>
     );
